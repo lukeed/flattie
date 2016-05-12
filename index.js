95190cff3a29ddbc1af758dbc136dcc4a6512591
luke <luke.edwards05@gmail.com>
2016-05-12 11:33:11 -0700

    plugin login, ignoring empty vals

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 'use strict';
-module.exports = function (str, opts) {
-	if (typeof str !== 'string') {
-		throw new TypeError('Expected a string');
+
+/**
+ * Flatten an Object into a single-depth Object
+ * @param  {Object} obj
+ * @param  {String} glue
+ * @return {Object}
+ */
+function flatObj(obj, glue) {
+	var out = {};
+	glue = glue || '_';
+
+	for (var k in obj) {
+		if ((typeof obj[k]) === 'object') {
+			var flat = flatObj(obj[k], glue);
+			for (var x in flat) {
+				if (flat.hasOwnProperty(x) && flat[x]) {
+					out[k + glue + x] = flat[x];
+				}
+			}
+		} else {
+			out[k] = obj[k];
+		}
 	}
+	return out;
+}
 
-	opts = opts || {};
-
-	return str + ' & ' + (opts.postfix || 'rainbows');
-};
+module.exports = flatObj;
