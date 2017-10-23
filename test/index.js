d4ec3953f1c5585b94d8931a345b957669a5ee58
Luke Edwards <luke.edwards05@gmail.com>
2017-10-23 01:46:44 -0700

    update tests; add fixtures inline

--- test/index.js
+++ test/index.js
@@ -1,5 +1,4 @@
 const test = require('tape');
-const fix = require('./fixtures');
 const fn = require('../lib');
 
 test('flat-obj', t => {
@@ -8,43 +7,35 @@ test('flat-obj', t => {
 });
 
 test('simple', t => {
-	t.deepEqual(fn(fix.foo), {
-		a: 'hello',
-		b: 'there',
-		c_breeze: 'sea',
-		c_saw: 'see',
-		d: 'end'
-	});
+	let c = { foo:'bar', baz:'bat' };
+	let val = fn({ a:1, b:2, c, d:4 });
+	t.deepEqual(val, { a:1, b:2, c_foo:'bar', c_baz:'bat', d:4 });
 	t.end();
 });
 
 test('custom glue', t => {
-	t.deepEqual(fn(fix.foo, 'HI'), {
-		a: 'hello',
-		b: 'there',
-		cHIbreeze: 'sea',
-		cHIsaw: 'see',
-		d: 'end'
-	});
+	let c = { x:7, y:8, z:9 };
+	let val = fn({ a:1, b:4, c, d:5 }, 'FOO');
+	t.deepEqual(val, { a:1, b:4, cFOOx:7, cFOOy:8, cFOOz:9, d:5 });
 	t.end();
 });
 
 test('ignore empty values', t => {
-	t.deepEqual(fn(fix.bar), {
-		a: 'hi',
-		b_d: 'hi'
+	let val = fn({
+		a: 123,
+		b: { a:null, b:undefined, c:'', d:0, e:'hi' },
+		c: null
 	});
+	t.deepEqual(val, { a:123, b_c:'', b_d:0, b_e:'hi' }, 'keeps 0 and empty string');
 	t.end();
 });
 
 test('multi-nested flattening', t => {
-	t.deepEqual(fn(fix.baz), {
-		a: 'hi',
-		b_a: 'hi',
-		b_b: 'yo',
-		b_c_a: 'howdy',
-		b_c_b: 'hello',
-		c: 'sup'
+	let val = fn({
+		a: 1,
+		b: { a:2, b:3, c:{ a:4, b:null, c:5 } },
+		c: 6
 	});
+	t.deepEqual(val, { a:1, b_a:2, b_b:3, b_c_a:4, b_c_c:5, c:6 });
 	t.end();
 });
