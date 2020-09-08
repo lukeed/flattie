80924729db57e6c11558b7fdd787cb3824c8e8cc
Luke Edwards <luke.edwards05@gmail.com>
2020-09-08 10:21:57 -0700

    fix: move non-object condition sooner;
    - common case first

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ function iter(output, sep, val, key) {
 	var k, pfx = key ? (key + sep) : key;
 
 	if (val == null) {
+	} else if (typeof val != 'object') {
+		output[key] = val;
 	} else if (Array.isArray(val)) {
 		for (k=0; k < val.length; k++) {
 			iter(output, sep, val[k], pfx + k);
 		}
-	} else if (typeof val == 'object') {
+	} else {
 		for (k in val) {
 			iter(output, sep, val[k], pfx + k);
 		}
-	} else {
-		output[key] = val;
 	}
 }
 
